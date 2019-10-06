3530f2759f809a9488c35eb4246d811b4802d7e2
rizkarhm <rizkarahma67@gmail.com>
2019-10-06 20:43:49 +0700

    supermarket

--- admin/home.php
+++ admin/home.php
@@ -6,8 +6,4 @@
 <div class="col-md-10">
     <h3>Selamat datang di</h3>
     <h3>Aplikasi Penjualan Sederhana :)</h3>
-    <br>
-    <br>
-    <br>
-    <p># Create Trigger - perubahan total setelah hapus barang pada transaksi masuk/keluar</p>
 </div>
